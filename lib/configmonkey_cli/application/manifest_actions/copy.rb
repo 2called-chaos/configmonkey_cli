ee704b1f64460c63e0fb7f01ee55aebf3f26e84f
Sven Pachnit <sven@bmonkeys.net>
2020-06-07 16:23:33 +0200

    Add `after_change` proc option to copy
    WARN: only works for files, not directories

--- lib/configmonkey_cli/application/manifest_actions/copy.rb
+++ lib/configmonkey_cli/application/manifest_actions/copy.rb
@@ -22,15 +22,31 @@ module ConfigmonkeyCli
 
         def destructive
           absolute_source = File.join(thor.source_paths[0], @source)
+          has_changed = !File.exist?(@destination)
+
+          if @opts[:after_change] && File.exist?(@destination)
+            has_changed = File.binread(absolute_source) != File.binread(@destination)
+          end
+
           if FileTest.directory?(absolute_source)
-            thor.directory(@source, @destination, @opts)
+            _perform_directory(@source, @destination, @opts)
           else
-            thor.copy_file(@source, @destination, @opts)
+            _perform_file(@source, @destination, @opts)
             if @opts[:chmod] && File.exist?(absolute_source) && File.exist?(@destination)
               mode = @opts[:chmod] == true ? File.stat(absolute_source).mode - 0100000 : @opts[:chmod]
               thor.chmod(@destination, mode) unless mode == File.stat(@destination).mode - 0100000
             end
           end
+
+          @opts[:after_change].call if has_changed && @opts[:after_change]
+        end
+
+        def _perform_directory(source, destination, opts)
+          thor.directory(source, destination, opts)
+        end
+
+        def _perform_file(source, destination, opts)
+          thor.copy_file(source, destination, opts)
         end
       end
     end
